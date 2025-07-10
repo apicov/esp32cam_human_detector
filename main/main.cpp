f2acdfb59b07fe33455bc949f492ecb06ccd87a5
pico <a.pico@posteo.de>
2025-07-10 11:16:33 +0200

    add tensorflow lite library

--- main/main.cpp
+++ main/main.cpp
@@ -22,6 +22,14 @@
 #include <MQTTClient.hpp>
 #include <WiFiStation.hpp>
 
+// Tensorflow lite micro
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/core/c/common.h"
+#include "tensorflow/lite/micro/micro_log.h"
+
 // shorten CONFIG names
 #define CONF(name) CONFIG_SNAP_ ## name
 
